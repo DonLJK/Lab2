836cde4f9e02b6c3d66b34ff59e4275a5ebc38ff
Don <donovanlimjk18.21@ichat.sp.edu.sg>
2022-10-26 16:21:00 +0800

    Lab 2 Ex7

--- Lab2.py
+++ Lab2.py
@@ -16,3 +16,21 @@
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to python")
+
+def display_main_menu():
+    print("Enter Some numbers separated by commas (e.g 5, 67, 32)")
+
+def calc_average():
+    print("calc_average")
+
+def get_user_input():
+    print("get_user_input")
+
+def find_min_max():
+    print("find_min_max")
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("Cal_Median_temp")
