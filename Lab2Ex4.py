f7cb9dfcd79231ac3bc3d6038715c3c533645dd9
Don <donovanlimjk18.21@ichat.sp.edu.sg>
2022-10-29 22:49:09 +0800

    Lab2Ex4

---
+++ Lab2Ex4.py
@@ -0,0 +1,52 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+#    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+def display_main_menu():
+    print("Enter Some numbers separated by spaces (e.g 5 67 32)")
+
+def calc_average(val2):
+    avg = sum(val2)/len(val2)
+    average = float(avg)
+    print(average)
+
+def get_user_input():
+    val = input("INPUT:")
+    val1 = val.split()
+    for i in range(len(val1)):
+        val1[i] = float(val1[i])
+    return val1
+
+
+def find_min_max(val2):
+    #print("find_min_max")
+    print("min :", min(val2))
+    print("max :", max(val2))
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temperature():
+    print("Cal_Median_temp")
+
+
+print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to python")
+display_main_menu()
+val2 = get_user_input()
+calc_average(val2)
+find_min_max(val2)
+
